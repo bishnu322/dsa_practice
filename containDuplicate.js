18affc8c80816ebc71488b8b18c5220d74e71415
bishnu_kohar <koharbishnu322@gmail.com>
2025-07-04 13:00:57 +0545

    first commit

---
+++ containDuplicate.js
@@ -0,0 +1,17 @@
+//? check whether the give array of element contains duplicate or not
+const checkDuplicate = (arr) => {
+  let obj = {};
+  arr.forEach((elem, i) => {
+    if (obj[elem]) {
+      obj[elem] = obj[elem] + 1;
+    } else {
+      obj[elem] = 1;
+    }
+  });
+
+  for (let key in obj) {
+    if (obj[key] > 1) return true;
+    return false;
+  }
+};
+console.log(checkDuplicate([1, 2, 3, 4, 1]));
