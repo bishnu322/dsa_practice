85196f36ec63f881bb36cb4e3d041bedf63b464f
bishnu_kohar <koharbishnu322@gmail.com>
2025-07-09 23:18:54 +0545

    remove duplicate

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -3,16 +3,19 @@ const arr = [1, 2, 2, 3, 4, 4, 5];
 const removeDuplicate = (data) => {
   const uniqueArr = [];
   const seenObj = {};
+
   for (let i = 0; i < data.length; i++) {
     let newData = data[i];
+
     if (!seenObj[newData]) {
       uniqueArr.push(newData);
       seenObj[newData] = true;
     }
   }
-  console.log(seenObj);
+
   return uniqueArr;
 };
+
 const data = removeDuplicate(arr);
 console.log(data);
 
