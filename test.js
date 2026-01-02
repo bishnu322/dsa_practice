630a2a6d6bde18c1aa6d11a2f5e7502002ae79d7
bishnu_kohar <koharbishnu322@gmail.com>
2026-01-02 10:48:39 +0545

    fetch method

--- test.js
+++ test.js
@@ -7,3 +7,12 @@ const numberOfLoopRepeat = (count) => {
   return count;
 };
 console.log(numberOfLoopRepeat(0));
+
+// while using fetch header method should be mentioned
+// fetch("https://api.myapp.com/login", {
+//   method: "POST",
+//   headers: {
+//     "content-type": "application/json",
+//   },
+//   body: JSON.stringify({ email: "sam@test.com" }),
+// });
